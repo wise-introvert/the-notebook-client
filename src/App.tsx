b64066b95b485bdbdd038957520870ef0e184ad0
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-10-05 18:52:11 +0530

    init apollo setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+import "./App.css";
+const LoginMutation = gql`
+  mutation Login($input: UserLoginInput!) {
+    login(input: $input) {
+      id
+      username
+    }
+  }
+`;
+
+const GetQuery = gql`
+  query {
+    departments {
+      id
+      name
+      courses {
+        id
+        name
+        subjects {
+          id
+          name
+          documents {
+            id
+            url
+            name
+          }
+        }
+      }
+    }
+  }
+`;
 
 function App() {
+  const [login, { data }] = useMutation(LoginMutation);
+  // const { loading, error, data: d } = useQuery(GetQuery);
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const onSubmit = (
+    e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>
+  ): void => {
+    e.preventDefault();
+    login({
+      variables: {
+        input: {
+          username,
+          password
+        }
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <span>
+        <label htmlFor={"username"}>Username</label>
+        <input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(e.target.value)
+          }
+          id={"username"}
+          name={"username"}
+          value={username}
+        />
+      </span>
+      <span>
+        <label htmlFor={"password"}>Password</label>
+        <input
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          id={"password"}
+          type={"password"}
+          name={"password"}
+          value={password}
+        />
+      </span>
+      <button type={"submit"} onClick={onSubmit}>
+        Submit
+      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </form>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  NormalizedCacheObject,
+  InMemoryCache
+} from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const link: any = createHttpLink({
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://the-notebook-graph.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
+  credentials: "include"
+});
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
